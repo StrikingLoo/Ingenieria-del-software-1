680b44e864f6bc1fbdb9d53aa71ec87abf0ab4c4
Luciano Strika <lucianostrika44@gmail.com>
2019-08-31 14:17:34 -0300

    implementadas todas las clases para modelar el problema, y los metodos necesarios. Falta la logica que mappea porosity y hardness a steps.

--- excavator/excavator.py
+++ excavator/excavator.py
@@ -4,8 +4,19 @@ class LunarRover():
         self.hardness = hardness
         self.excavatingProtocol = []
 
+    def __init__(self, porosity, hardness, protocol):
+        self.porosity = porosity
+        self.hardness = hardness
+        self.excavatingProtocol = protocol
+
+    def execute(self):
+        print('starting process...')
+        for step in self.excavatingProtocol:
+            step.execute()
+        print('process finished successfully!')
+
     def __str__(self):
-        return f'a Rover with porosity={self.porosity} hardness={self.hardness}'
+        return f'a Rover with porosity={self.porosity} hardness={self.hardness} and protocol {self.excavatingProtocol}'
 
 class Drill():
     def __init__(self, clockwise, speed, duration_in_seconds):
@@ -16,9 +27,9 @@ class Drill():
     def execute(self):
         message = f'the drill drills with a speed of {self.speed} for {self.duration_in_seconds} seconds'
         if self.clockwise:
-            message+=' clockwise'
+            message+=' clockwise.'
         else:
-            message+=' counter-clockwise'
+            message+=' counter-clockwise.'
         print(message)
     
     def __str__(self):
@@ -28,10 +39,38 @@ class Drill():
         else:
             rep+=', it drills counter-clockwise'
         return rep
-        
-        
 
-lr = LunarRover(100,5)
+class Claw():
+    def __init__(self, isOpen = True):
+        self.isOpen = isOpen
+
+    def actionMessage(self):
+        if self.isOpen:
+            return 'closed'
+        else:
+            return 'opened'
+
+    def switch(self):
+        self.isOpen = not self.isOpen
+
+    def execute(self):
+        action = self.actionMessage()
+        self.switch()
+        message = f'the claw is {action}'
+        
+        print(message)
+    
+    def __str__(self):
+        rep = f'a Claw that is'
+        rep+= ' open' if self.isOpen else ' closed'
+        return rep
+
+
+lr = LunarRover(100,5, [])
 d = Drill(True, '15RPM',100)
+d2 = Drill(False, '45RPM',100)
+claw = Claw()
 print(d)
 print(lr)
+lr2 = LunarRover(100,5,[d,d2,claw])
+lr2.execute()
